ef894ef7144a500358fdf27c3c36ab3c80daf29f
Isaías González <ijgggr@gmail.com>
2026-01-30 14:00:26 -0800

    deploy

---
+++ README.md
@@ -0,0 +1,9 @@
+# C2 - All For One API
+
+- Author: Isaias Gonzalez
+- Revised: 2026-01-30
+- Link: https://csa-2526-ig-afo-api-b7ccczbnakheg9e9.westus3-01.azurewebsites.net/
+
+## Peer Review
+
+- TODO
--- SV/Controllers/HelloWorldController.cs
+++ SV/Controllers/HelloWorldController.cs
@@ -5,18 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SV.Services;
 
-//// hello world
-//// asking questions
-//// adding two numbers
-//// madlib
-//// odd or even
-//// reverse it (alphanumeric)
-//// reverse it (numeric)
-//// magic 8 ball
-//// guess it
-//// restaurant picker
-// step-by-step pdf for creating a hello world endpoint via swagger (detailed explanations and images)
-
 namespace SV.Controllers
 {
     [ApiController]
